7391b65f93abadd496b9138449a268fbedede9f0
Carrie Bailey <siuslawcarrie@gmail.com>
2023-11-14 09:49:37 -0800

    merging I think midterm practice problems & midterm operators

--- EOU 221 Mooney game/program_functions.cpp
+++ EOU 221 Mooney game/program_functions.cpp
@@ -1,12 +1,12 @@
+////
+//// Created by siusl on 11/9/2023.
+////
+//#include "program_functions.hpp"
+//void display_game_state(string Game_Location, string P1_Name, string P2_Name, int P1_Health, int P2_Health){
 //
-// Created by siusl on 11/9/2023.
+//    //function code
+//}
 //
-#include "program_functions.hpp"
-void display_game_state(string Game_Location, string P1_Name, string P2_Name, int P1_Health, int P2_Health){
-
-    //function code
-}
-
-void write_game_state(string Game_Location, string P1_Name, string P2_Name, int P1_Health, int P2_Health, ofstream out){
-    //function code
-}
+//void write_game_state(string Game_Location, string P1_Name, string P2_Name, int P1_Health, int P2_Health, ofstream out){
+//    //function code
+//}
